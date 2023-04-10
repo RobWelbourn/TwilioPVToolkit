021caedad15260e925bf30152af1b48e53d1d49a
RobWelbourn <robert@welbourn.com>
2023-04-10 19:52:04 -0400

    Add util/phonenumbers.js

---
+++ utils/phonenumbers.js
@@ -0,0 +1,23 @@
+/**
+ * @module utils/phonenumbers
+ * 
+ * Module that provides a wrapper around libphonenumber-js to allow parsing of phone numbers using the 
+ * local machine's locale as the default.
+ * @see {@link https://gitlab.com/catamphetamine/libphonenumber-js}
+ */
+
+import parsePhoneNumber from 'libphonenumber-js';
+
+const locale = Intl.DateTimeFormat().resolvedOptions().locale;  // e.g. en-US
+const localeCountry = locale.split('-')[1];
+
+/**
+ * Parses a phone number, possibly including spaces and punctuation, taking into account local dialing conventions.
+ * @param {string} pn - The phone number to parse
+ * @param {string} {countryCode} - Two-letter ISO 3166 country code; defaults to locale of local machine
+ * @returns {string} - E.164 (international) format phone number, with no punctuation
+ */
+export function parsePN(pn, countryCode) {
+    countryCode = countryCode ? countryCode.toUpperCase() : localeCountry;
+    return parsePhoneNumber(pn, countryCode).number;
+}
