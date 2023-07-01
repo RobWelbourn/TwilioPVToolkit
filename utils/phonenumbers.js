f85d6ca07cc8e827d8294c1572e282e57a4c94bf
RobWelbourn <robert@welbourn.com>
2023-07-01 18:32:01 -0400

    Added function to get national format from E.164 number

--- utils/phonenumbers.js
+++ utils/phonenumbers.js
@@ -3,7 +3,7 @@
  * 
  * Module that provides a wrapper around libphonenumber-js to allow parsing of phone numbers using the 
  * local machine's locale as the default.
- * @see {@link https://gitlab.com/catamphetamine/libphonenumber-js}
+ * @see {@link https://gitlab.com/catamphetamine/libphonenumber form-js}
  */
 
 import parsePhoneNumber from 'libphonenumber-js';
@@ -20,4 +20,13 @@ const localeCountry = locale.split('-')[1];
 export function parsePN(pn, countryCode) {
     countryCode = countryCode ? countryCode.toUpperCase() : localeCountry;
     return parsePhoneNumber(pn, countryCode).number;
+}
+
+/**
+ * Returns a phone number in national, friendly format.
+ * @param {string} pn - The phone number in E.164 format
+ * @returns {string} - The phone number in national format
+ */
+export function nationalPN(pn) {
+    return parsePhoneNumber(pn).formatNational();
 }
